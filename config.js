a4990af140d13cbbca1e7484822dc977a083170c
aviadc91 <158831162+aviadc91@users.noreply.github.com>
2026-06-03 15:12:55 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+// ============================================================
+//  config.js — הגדרות Firebase
+// ============================================================
+const FIREBASE_CONFIG = {
+  apiKey:            "AIzaSyAerXwbgc6bEiLCiyKZpsaHTumztV4H9Eg",
+  authDomain:        "be-service---aviad-cohen.firebaseapp.com",
+  projectId:         "be-service---aviad-cohen",
+  storageBucket:     "be-service---aviad-cohen.firebasestorage.app",
+  messagingSenderId: "1018408465237",
+  appId:             "1:1018408465237:web:76ee064a91a427730a75dc"
+};
